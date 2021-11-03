4d52658445f004310a063e6f7cfa24fc82fdc457
Susana Lima <93327294+SusanaLima55p@users.noreply.github.com>
2021-11-03 10:19:32 -0300

    organizando

--- aula06/ex003/js/app.js
+++ aula06/ex003/js/app.js
@@ -1,2 +1,6 @@
-var nome = window.prompt('Qual é o seu nome?')
-alert('É um grande prazer em te conhecer, ' + nome+ '!' )
+var n1 = window.prompt('Digite um número: ')//string
+var n2 = window.prompt('Digite outro número: ')//string
+var s = n1 + n2 //string
+window.alert('A soma dos valores é ' + s)//concatenação
+//(number + number) para adição
+//(string + string) para concatenação
