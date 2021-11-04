ef3121d9adffc71cf0720bccc8b06e534fcf6a23
Susana Lima <93327294+SusanaLima55p@users.noreply.github.com>
2021-11-04 11:00:58 -0300

    d002

---
+++ desafios/d002/doc.js
@@ -0,0 +1,13 @@
+var inicio = window.document.getElementById('inicio')
+inicio.addEventListener('click', iniciar)
+
+function iniciar(){
+    var nome = window.prompt('Qual é o seu nome?')
+
+    var idade = window.prompt(`Olá, ${nome}! Quantos anos você tem?`)
+
+    window.alert(`Acabei de conhecer ${nome}, que tem ${idade} anos de idade!`)
+}
+
+
+
