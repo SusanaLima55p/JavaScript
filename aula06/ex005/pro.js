c89ca81e34c6afb9b51dc3a8a4d9231cbab2b41a
Susana Lima <93327294+SusanaLima55p@users.noreply.github.com>
2021-11-04 09:33:40 -0300

    ok

---
+++ aula06/ex005/pro.js
@@ -0,0 +1,10 @@
+/*var p1 = window.document.getElementsByTagName('p')[0]
+window.document.write('Está escrito assim: ' +p1.innerText)
+p1.style.color = 'blue'
+var corpo = window.document.body
+corpo.style.background = 'black'
+var d = window.document.getElementById('msg')
+d.style.background = 'green'
+d.innerText = 'Estou aguardando...'*/
+var d = document.querySelector('div#msg')
+d.style.color = ' blue'
