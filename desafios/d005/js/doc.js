d43b221fdd1af791976629120b4dedee18187d01
Susana Lima <93327294+SusanaLima55p@users.noreply.github.com>
2021-11-09 08:24:24 -0300

    ok

--- desafios/d005/js/doc.js
+++ desafios/d005/js/doc.js
@@ -8,7 +8,7 @@ function clicar ( )  {
   var me = prompt('Digite uma distância em metros (m)')
   var res = window.document.getElementById('res')
   res.innerHTML  =  `<h2> A distância ${me.replace('.', ',')} metros corresponde a ... </h2>`
-  res.innerHTML  +=  `<p> ${ ( me  /  1000 ) . toLocaleString ( 'pt-br' ,  { style : 'decimal' } ) } milhas (Km). </p> `
+  res.innerHTML  +=  `<p> ${ ( me  /  1000 ) . toLocaleString ( 'pt-br' ,  { style : 'decimal' } ) } quilômetros (Km). </p> `
   res.innerHTML  +=  `<p> ${ ( me  /  100 ) . toLocaleString ( 'pt-br' ,  { style : 'decimal' } ) } hectômetros (Hm). </p> `
   res.innerHTML  +=  `<p> ${ ( me  /  10 ) . toLocaleString ( 'pt-br' ,  { style : 'decimal' } ) } decâmetros (Dam). </p> `
   res.innerHTML  +=  `<p> ${ ( me  *  10 ) . toLocaleString ( 'pt-br' ,  { style : 'decimal' } ) } decímetros (dm). </p> `
