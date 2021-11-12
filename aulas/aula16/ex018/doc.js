c464ab331df557ed7c90dc6fb40131853334e8bf
Susana Lima <93327294+SusanaLima55p@users.noreply.github.com>
2021-11-12 16:18:05 -0300

    ok

--- aulas/aula16/ex018/doc.js
+++ aulas/aula16/ex018/doc.js
@@ -1,10 +1,10 @@
-
-    var nu = document.getElementById('nu')
-    var val = []
+var nu = document.getElementById('nu')
+var numeros = document.getElementById('numeros')
+var inf = document.getElementById('inf')
+var val = []
 
 function adi(){
-    var numeros = document.getElementById('numeros')
-    var nu1 = nu.value
+    var nu1 = Number(nu.value)
     //buscar o valor no vetor
     function bus (nu1){
             res = val.indexOf(nu1)
@@ -14,34 +14,45 @@ function adi(){
     if(nu.value.length == 0){
         alert('Digite um número!')
     //numero entre 1 e 100
-    } else if(nu1 >= 1 && nu1 < 100) {
+    } else if(nu1 >= 1 && nu1 <= 100) {
         bus(nu1)
         //validação de indice
         if(res == -1){
             val.push(nu1)
             numeros.innerHTML += `<option value="">Número ${nu1} adicionado</option>`
+            inf.innerHTML = ''
         } else {
-            alert(`O número ${nu1} já foi adicionado`)}
+            alert(`O número ${nu1} já foi adicionado`)
+        }
     //numero que não está entre 1 e 100
     } else{
-      alert('escreva um número entre 1 e 100')}
+      alert('escreva um número entre 1 e 100')
+    }
+      nu.value =  ''
+      nu.focus()
 }
 
-var inf = document.getElementById('inf')
+
 function fim(){
-    val.sort()
+    if(val.length == 0){
+        alert('Digite um número!')
+    } else{
+        var soma = 0
+        var maior = val[0]
+        var menor = val[0]
+        for(var pos in val){
+            soma += (val[pos])
+            if(val[pos] > maior)
+            maior = val[pos]
+            if(val[pos] < menor)
+            menor = val[pos]
+        }
+        var media = soma / val.length
 
-    soma = 0
-    for(var pos in val){
-        var nu = Number(val[pos])
-        var ult = (val[pos])
-        soma += nu
+        inf.innerHTML = `<p>Ao todo, temos ${val.length} números cadastrados</p>`
+        inf.innerHTML += `<p>O maior número informado foi ${maior}</p>`
+        inf.innerHTML += `<p>O menor número informado foi ${menor}</p>`
+        inf.innerHTML += `<p>Somando todos os valores temos ${soma}</p>`
+        inf.innerHTML +=`<p>A média dos valores é ${media.toFixed(2).replace('.', ',')}</p>`
     }
-    var media = soma / val.length
-
-    inf.innerHTML = `<p>Ao todo, temos ${val.length} números cadastrados</p>`
-    inf.innerHTML += `<p>O maior número informado foi ${ult}</p>`
-    inf.innerHTML += `<p>O menor número informado foi ${val[0]}</p>`
-    inf.innerHTML += `<p>Somando todos os valores temos ${soma}</p>`
-    inf.innerHTML +=`<p>A média dos valores é ${media.toFixed(2)}</p>`
 }
